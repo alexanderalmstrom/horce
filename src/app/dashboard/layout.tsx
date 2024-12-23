a1062ee66c3338a9f169fdc865794c8a45524112
Alexander Almström <info@alexanderalmstrom.com>
2024-12-23 15:49:11 +0100

    Add suspense loading for profile details

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>{children}</main>;
+  return <main className="p-4">{children}</main>;
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,22 @@
+import { Suspense } from "react";
 import CurrentUserProfile from "../_components/CurrentUserProfile";
 
 export default function Page() {
   return (
     <>
       <h1 className="text-xl">Dashboard</h1>
-      <CurrentUserProfile />
+      <Suspense
+        fallback={
+          <div className="flex flex-col items-start gap-2 py-1">
+            <div className="h-4 w-full max-w-8 animate-pulse rounded-sm bg-foreground/10" />
+            <div className="h-4 w-full max-w-64 animate-pulse rounded-sm bg-foreground/10" />
+            <div className="h-4 w-full max-w-24 animate-pulse rounded-sm bg-foreground/10" />
+            <div className="h-4 w-full max-w-16 animate-pulse rounded-sm bg-foreground/10" />
+          </div>
+        }
+      >
+        <CurrentUserProfile />
+      </Suspense>
     </>
   );
 }
