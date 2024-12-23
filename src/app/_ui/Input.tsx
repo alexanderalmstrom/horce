f1325994499c2de96f6ff232fab2ffeb5382e5ee
Alexander Almström <info@alexanderalmstrom.com>
2024-12-23 15:35:09 +0100

    Run prettier

--- src/app/_ui/Input.tsx
+++ src/app/_ui/Input.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from "react";
 export default function Input(props: ComponentProps<"input">) {
   return (
     <input
-      className="placeholder:text-foreground-accent rounded-md border border-foreground/15 bg-background-accent px-4 py-2 outline-none transition-[border,background-color] focus-within:border-foreground/50 group-hover:[&:not(:focus)]:border-foreground/25"
+      className="rounded-md border border-foreground/15 bg-background-accent px-4 py-2 outline-none transition-[border,background-color] placeholder:text-foreground-accent focus-within:border-foreground/50 group-hover:[&:not(:focus)]:border-foreground/25"
       {...props}
     />
   );
