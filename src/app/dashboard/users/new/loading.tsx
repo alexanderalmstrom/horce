c4340253abaa3200bbc9015e27a82d37a9d5841a
Alexander Almström <info@alexanderalmstrom.com>
2025-01-08 21:11:51 +0100

    Update max widths

--- src/app/dashboard/users/new/loading.tsx
+++ src/app/dashboard/users/new/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex max-w-5xl flex-col items-start gap-5 py-1">
+    <div className="flex flex-col items-start gap-5 py-1">
       <div className="h-6 w-full max-w-24 animate-pulse rounded-sm bg-foreground/10" />
       <div className="flex w-full flex-col gap-2">
         <div className="h-4 w-full max-w-16 animate-pulse rounded-sm bg-foreground/10" />
@@ -10,7 +10,12 @@ export default function Loading() {
         <div className="h-4 w-full max-w-16 animate-pulse rounded-sm bg-foreground/10" />
         <div className="h-10 w-full animate-pulse rounded-sm bg-foreground/10" />
       </div>
+      <div className="flex w-full flex-col gap-2">
+        <div className="h-4 w-full max-w-16 animate-pulse rounded-sm bg-foreground/10" />
+        <div className="h-10 w-full animate-pulse rounded-sm bg-foreground/10" />
+      </div>
       <div className="h-12 w-full animate-pulse rounded-sm bg-foreground/20" />
+      <div className="h-12 w-full animate-pulse rounded-sm bg-foreground/10" />
     </div>
   );
 }
