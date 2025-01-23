6fcc86c56001eaf22c3b542d9b53a6b35c8759c0
Alexander Almström <info@alexanderalmstrom.com>
2025-01-23 21:29:54 +0100

    Move loading skeleton to suspense

--- src/app/dashboard/users/layout.tsx
+++ src/app/dashboard/users/layout.tsx
@@ -1,11 +1,30 @@
-import type { PropsWithChildren } from "react";
+import { Suspense, type PropsWithChildren } from "react";
 import UsersList from "~/app/_components/UsersList";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <h1 className="text-2xl">Users</h1>
-      <UsersList />
+      <Suspense
+        fallback={
+          <div className="flex flex-col items-start gap-4 py-6">
+            <div className="h-6 w-full max-w-24 animate-pulse rounded-sm bg-foreground/10" />
+            <div className="flex h-4 w-full flex-row gap-3">
+              <div className="w-full max-w-32 animate-pulse rounded-sm bg-foreground/10" />
+              <div className="w-full max-w-32 animate-pulse rounded-sm bg-foreground/10" />
+              <div className="w-full max-w-32 animate-pulse rounded-sm bg-foreground/10" />
+            </div>
+            <div className="flex w-full flex-col gap-2">
+              <div className="h-4 w-full animate-pulse rounded-sm bg-foreground/10" />
+              <div className="h-4 w-full animate-pulse rounded-sm bg-foreground/10" />
+              <div className="h-4 w-full animate-pulse rounded-sm bg-foreground/10" />
+            </div>
+            <div className="h-12 w-full animate-pulse rounded-sm bg-foreground/20" />
+          </div>
+        }
+      >
+        <UsersList />
+      </Suspense>
       {children}
     </>
   );
