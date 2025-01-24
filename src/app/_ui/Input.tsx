6cae8bf78f7b094917be1fdd1e7b30cc5c5ade6f
Alexander Almström <info@alexanderalmstrom.com>
2025-01-24 22:20:01 +0100

    Input placeholder text color

--- src/app/_ui/Input.tsx
+++ src/app/_ui/Input.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from "react";
 export default function Input(props: ComponentProps<"input">) {
   return (
     <input
-      className="rounded-md border border-foreground/15 bg-background-accent px-4 py-2 outline-none transition-[border,background-color] placeholder:text-foreground-accent focus-within:border-foreground/50 group-hover:[&:not(:focus)]:border-foreground/25"
+      className="rounded-md border border-foreground/15 bg-background-accent px-4 py-2 outline-none transition-[border,background-color] placeholder:text-foreground-accent placeholder:text-foreground/50 focus-within:border-foreground/50 group-hover:[&:not(:focus)]:border-foreground/25"
       {...props}
     />
   );
